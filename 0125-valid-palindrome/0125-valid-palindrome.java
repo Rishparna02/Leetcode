32865d9956ba5bfc7f3d90579e31a9ddce5885a8
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2026-03-26 23:33:04 +0530

    Time: 4 ms (49.28%), Space: 45.1 MB (43.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,29 +1,11 @@
-class Solution {
+public class Solution {
     public boolean isPalindrome(String s) {
-        // Initialize two pointers
-        int left = 0;
-        int right = s.length() - 1;
-        
-        while (left < right) {
-            // Move the left pointer to the next alphanumeric character
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
+        StringBuilder newStr = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                newStr.append(Character.toLowerCase(c));
             }
-            // Move the right pointer to the previous alphanumeric character
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-            
-            // Compare the characters at the left and right pointers
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-                return false;
-            }
-            
-            // Move both pointers inward
-            left++;
-            right--;
         }
-        
-        return true; // All characters matched, it's a palindrome
+        return newStr.toString().equals(newStr.reverse().toString());
     }
 }
