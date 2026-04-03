762feb5428f8922662e9f0b0259492445cf7428c
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2026-04-03 13:52:38 +0530

    Time: 0 ms (100%), Space: 43.7 MB (68.49%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,15 @@
-public class Solution {
+class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Pointers for nums1, nums2, and the merged array
-        int p1 = m - 1;  // last element in the non-zero part of nums1
-        int p2 = n - 1;  // last element in nums2
-        int p = m + n - 1;  // last position in nums1
+        int last = m + n - 1;
+        int i = m - 1; //last valid elements in nums1
+        int j = n - 1; //last element in nums2.
 
-        // Merge the arrays from the end
-        while (p1 >= 0 && p2 >= 0) {
-            // Compare the elements from the end and place the larger one
-            if (nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1];
-                p1--;
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[last--] = nums1[i--];
             } else {
-                nums1[p] = nums2[p2];
-                p2--;
+                nums1[last--] = nums2[j--];
             }
-            p--;
-        }
-
-        // If there are remaining elements in nums2, copy them
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2];
-            p2--;
-            p--;
         }
     }
 }
