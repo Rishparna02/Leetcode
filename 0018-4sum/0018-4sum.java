6a831f8f7557187e9adb3fb85a81e881414d466c
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2026-04-05 12:15:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,50 +1,42 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+public class Solution {
+    private List<List<Integer>> res;
+    private List<Integer> quad;
 
-class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        if (nums == null || nums.length < 4) return result;
-
-        // Sort the array to enable the two-pointer technique
         Arrays.sort(nums);
-        
-        int n = nums.length;
-        
-        for (int i = 0; i < n - 3; i++) {
-            // Avoid duplicates for the first element
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        res = new ArrayList<>();
+        quad = new ArrayList<>();
+        kSum(nums, 4, 0, target);
+        return res;
+    }
 
-            for (int j = i + 1; j < n - 2; j++) {
-                // Avoid duplicates for the second element
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int left = j + 1;
-                int right = n - 1;
-
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-                    
-                    if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
-                        // Move left and right pointers to avoid duplicates
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-
-                        left++;
-                        right--;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
+    private void kSum(int[] nums, int k, int start, long target) {
+        if (k == 2) {
+            int l = start, r = nums.length - 1;
+            while (l < r) {
+                long sum = nums[l] + nums[r];
+                if (sum < target) {
+                    l++;
+                } else if (sum > target) {
+                    r--;
+                } else {
+                    res.add(new ArrayList<>(quad));
+                    res.get(res.size() - 1).add(nums[l]);
+                    res.get(res.size() - 1).add(nums[r]);
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
                 }
             }
+            return;
         }
-        
-        return result;
+
+        for (int i = start; i < nums.length - k + 1; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            quad.add(nums[i]);
+            kSum(nums, k - 1, i + 1, target - nums[i]);
+            quad.remove(quad.size() - 1);
+        }
     }
 }
