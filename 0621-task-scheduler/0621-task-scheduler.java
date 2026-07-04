834392569be9cf06a3e61289c442a834584378d2
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2026-07-04 11:43:37 +0530

    Time: 3 ms (83.1%), Space: 48.6 MB (11.85%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,18 +1,20 @@
-public class Solution {
+class Solution {
     public int leastInterval(char[] tasks, int n) {
+        
+
+        //counting the number of tasks 
         int[] count = new int[26];
-        for (char task : tasks) {
+
+        for(char task : tasks) {
             count[task - 'A']++;
         }
-
         int maxf = Arrays.stream(count).max().getAsInt();
         int maxCount = 0;
-        for (int i : count) {
-            if (i == maxf) {
+        for(int i : count) {
+            if(i == maxf) {
                 maxCount++;
             }
         }
-
         int time = (maxf - 1) * (n + 1) + maxCount;
         return Math.max(tasks.length, time);
     }
