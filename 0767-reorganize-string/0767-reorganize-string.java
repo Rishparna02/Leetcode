bce2f1a1e4387f50965717fcfd17d92c549908b0
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2026-07-08 16:25:08 +0530

    Time: 5 ms (38.49%), Space: 43.3 MB (24.68%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,45 +1,33 @@
-public class Solution {
+class Solution {
     public String reorganizeString(String s) {
         int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
+        for(char c : s.toCharArray()) {
             freq[c - 'a']++;
         }
-
-        int maxFreq = Arrays.stream(freq).max().getAsInt();
-        if (maxFreq > (s.length() + 1) / 2) {
-            return "";
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > 0) {
+                maxHeap.offer(new int[]{freq[i], i});
+            }
         }
-
         StringBuilder res = new StringBuilder();
-        while (res.length() < s.length()) {
-            int maxIdx = findMaxIndex(freq);
-            char maxChar = (char) (maxIdx + 'a');
-            res.append(maxChar);
-            freq[maxIdx]--;
-
-            if (freq[maxIdx] == 0) {
-                continue;
+        int[] prev = null;
+        while(!maxHeap.isEmpty() || prev != null) {
+            if(prev != null && maxHeap.isEmpty()) {
+                return "";
             }
+            int[] curr = maxHeap.poll();
+            res.append((char) (curr[1] + 'a'));
+            curr[0]--;
 
-            int tmp = freq[maxIdx];
-            freq[maxIdx] = Integer.MIN_VALUE;
-            int nextMaxIdx = findMaxIndex(freq);
-            char nextMaxChar = (char) (nextMaxIdx + 'a');
-            res.append(nextMaxChar);
-            freq[maxIdx] = tmp;
-            freq[nextMaxIdx]--;
+            if(prev != null){
+                maxHeap.offer(prev);
+                prev = null;
+            }
+            if(curr[0] > 0) {
+                prev = curr;
+            }
         }
-
         return res.toString();
-    }
-
-    private int findMaxIndex(int[] freq) {
-        int maxIdx = 0;
-        for (int i = 1; i < freq.length; i++) {
-            if (freq[i] > freq[maxIdx]) {
-                maxIdx = i;
-            }
-        }
-        return maxIdx;
     }
 }
