e7e5dc25c4e4fc48b7525534a6176b6f8547076e
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2026-08-07 18:14:33 +0530

    Time: 0 ms (100%), Space: 47.8 MB (56.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,5 +1,8 @@
 class Solution {
     public int trap(int[] height) {
+        if(height == null || height.length == 0) {
+            return 0;
+        }
         int l = 0, r = height.length - 1;
 
         int leftMax = height[l];
