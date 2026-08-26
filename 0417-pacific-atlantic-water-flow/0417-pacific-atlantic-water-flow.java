d805e551fe1c2df363fc7096d98b5dda432d7657
Rishparna Bandopadhyay <109991962+Rishparna02@users.noreply.github.com>
2026-08-26 14:22:20 +0530

    Time: 4 ms (98.41%), Space: 47 MB (98.12%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -5,44 +5,33 @@ class Solution {
         boolean[][] pac = new boolean[ROWS][COLS];
         boolean[][] atl = new boolean[ROWS][COLS];
 
-        Queue<int[]> pacQueue = new LinkedList<>();
-        Queue<int[]> atlQueue = new LinkedList<>();
-
-        for (int c = 0; c < COLS; c++) {
-            pacQueue.add(new int[]{0, c});
-            atlQueue.add(new int[]{ROWS - 1, c});
+        for(int c = 0; c < COLS; c++) {
+            dfs(0, c, pac, heights);
+            dfs(ROWS - 1, c, atl, heights);
         }
-        for (int r = 0; r < ROWS; r++) {
-            pacQueue.add(new int[]{r, 0});
-            atlQueue.add(new int[]{r, COLS - 1});
+        for(int r = 0; r < ROWS; r++) {
+            dfs(r, 0, pac, heights);
+            dfs(r, COLS - 1, atl, heights);
         }
-
-        bfs(pacQueue, pac, heights);
-        bfs(atlQueue, atl, heights);
-
         List<List<Integer>> res = new ArrayList<>();
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (pac[r][c] && atl[r][c]) {
+        for(int r = 0; r < ROWS; r++) {
+            for(int c = 0; c < COLS; c++) {
+                if(pac[r][c] && atl[r][c]) {
                     res.add(Arrays.asList(r, c));
                 }
             }
         }
         return res;
+        
     }
-
-    private void bfs(Queue<int[]> q, boolean[][] ocean, int[][] heights) {
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0], c = cur[1];
-            ocean[r][c] = true;
-            for (int[] d : directions) {
-                int nr = r + d[0], nc = c + d[1];
-                if (nr >= 0 && nr < heights.length && nc >= 0 &&
-                    nc < heights[0].length && !ocean[nr][nc] &&
-                    heights[nr][nc] >= heights[r][c]) {
-                    q.add(new int[]{nr, nc});
-                }
+    private void dfs(int r, int c, boolean[][] ocean, int[][] heights) {
+        ocean[r][c] = true;
+        for(int[] d : directions) {
+            int nr = r + d[0], nc = c + d[1];
+            if(nr >= 0 && nr < heights.length && 
+            nc >= 0 && nc < heights[0].length && 
+            !ocean[nr][nc] && heights[nr][nc] >= heights[r][c]) {
+                dfs(nr, nc, ocean, heights);
             }
         }
     }
